e6bb8da5d983f7106cf83cb0cdf3472233930613
KyleMattD <kyledrotsky@gmail.com>
2021-01-08 22:40:36 -0800

    first commit

---
+++ src/app/Models/Learner.ts
@@ -0,0 +1,9 @@
+export class Learner{
+    constructor(){}
+    public Learner_ID: string;
+    public Learner_Address : string;
+    public Learner_DOB: Date;
+    public Learner_School: string; 
+    public Learner_Grade: string;
+    public User_Docx: string;
+}
---
+++ src/app/Models/Register.ts
@@ -0,0 +1,18 @@
+export class Register{
+    constructor(){}
+    public Email: string;
+    public Password: string;
+    public Name: string;
+    public Surname: string;
+    public School: string;
+    public Address : string;
+    public DOB: Date;
+}
+
+export class User{
+    constructor(){}
+    public User_Email: string;
+    public User_Type_ID: string;
+    public User_Name: string;
+    public User_Surname: string;
+}

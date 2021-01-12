a2c71dda492b2eb34c3d506776e40c494f301db2
KyleMattD <kyledrotsky@gmail.com>
2021-01-12 08:17:23 -0800

    new changes

--- src/app/Register/register.component.ts
+++ src/app/Register/register.component.ts
@@ -5,7 +5,7 @@ import { emailValidator, matchingPasswords } from '../theme/utils/app-validators
 import { AppSettings } from '../app.settings';
 import { Settings } from '../app.settings.model';
 import { Register } from '../Models/Register';
-import { AngularFireStorage, AngularFireUploadTask  } from '@angular/fire/storage';   //   import <<<<
+import { AngularFireStorage, AngularFireUploadTask  } from '@angular/fire/storage'; 
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 @Component({
@@ -23,21 +23,13 @@ export class RegisterComponent {
   submit1: boolean = false;
   submit2: boolean = false;
 
-
-
-
-
-
   constructor(public appSettings:AppSettings, public fb: FormBuilder, public router:Router,private fireStorage: AngularFireStorage,private http: HttpClient){
     this.settings = this.appSettings.settings; 
     this.form = this.fb.group({
       'name': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
       'surname': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
-      'id': [null, Validators.compose([Validators.required, Validators.minLength(13)])],
-      'number': [null, Validators.compose([Validators.required, Validators.minLength(10)])],
-      'city': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
-      'address': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
-      'postalCode': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      'grade': [null, Validators.compose([Validators.required, Validators.minLength(13)])],
+      'school': [null, Validators.compose([Validators.required, Validators.minLength(10)])],
       'email': [null, Validators.compose([Validators.required, emailValidator])],
       'password': ['', Validators.required],
       'confirmPassword': ['', Validators.required]
@@ -45,75 +37,59 @@ export class RegisterComponent {
   }
 
 
- SubmitData = new Register(); //  <<< Submitted from the form
- PostData = new Register();   //  <<< To be Submitted to the API
+ SubmitData = new Register(); 
+ PostData = new Register();   
 
-file: any; // <<< Photo placeholder to be sent to firebase
-progressValue: Observable<number>; //   <<<  Progress bar for image upload
+file: any; 
+progressValue: Observable<number>; 
 
 //=================================Initial submit for image upload to firebase========================
   public async onSubmit() {
 
-     
     if (this.file) {
-      
-      
-       const filePath = `${this.basePath}/${this.SubmitData.IDNum}`;  // path at which image will be stored in the firebase storage
-       this.task =  this.fireStorage.upload(filePath, this.file);    // upload task
+       const filePath = `${this.basePath}/${this.SubmitData.Email}`;  
+       this.data =  this.fireStorage.upload(filePath, this.file);    
  
-       // this.progress = this.snapTask.percentageChanges();
- 
-       this.progressValue = this.task.percentageChanges();
-       (await this.task).ref.getDownloadURL().then(url => 
+       this.progressValue = this.data.percentageChanges();
+       (await this.data).ref.getDownloadURL().then(url => 
         {
-          this.downloadableURL = url;// <<< url is found here
-          this.postData(); //This is calling your final post for the rest of the info
+          this.downloadableURL = url;
+          this.postData(); 
         });  
      } else {  
-       this.downloadableURL = ''; //IF no picture is uploaded
+       this.downloadableURL = ''; 
        this.postData();
       }
- 
- 
-       
    }
 
-   //=================================Secondary functio to submit to API===========================
   postData(){
-    
-    this.PostData = this.SubmitData;
-    this.PostData.Image = this.downloadableURL;   //   <<< Set firebase URl for database
+    this.PostData = this.SubmitData; 
     this.http.post<Register>("http://localhost:4200/api/User/Register", this.PostData).subscribe(
       success=> {
         alert("Registration succesfull!");
         this.router.navigateByUrl("/login");
-
       }
     );
   }
-//=================================================================================================
+
   onSelectedFile(event){
-    
     const reader = new FileReader();
-    
     if(event.target.files && event.target.files.length) {
       const [file] = event.target.files;
       reader.readAsDataURL(file);
     
       reader.onload = () => {
    
-        this.imageSrc = reader.result as string;
+        this.DocSrc = reader.result as string;
      
         this.form.patchValue({
           fileSource: reader.result
         });
-   
       };
    
     }
-    
        this.file = event.target.files[0];
-       this.file.name = this.SubmitData.IDNum;
+       this.file.name = this.SubmitData.Email;
      
     
   }
@@ -123,64 +99,55 @@ data:any;
   checkEmail(){
 
     if(this.SubmitData.Email.length >= 10){
-    this.http.post("http://localhost:4200/api/UserExists", this.SubmitData.Email).subscribe(  //  <<< Post for ID exists check
+    this.http.post("http://localhost:4200/api/UserExists", this.SubmitData.Email).subscribe(  
       success => {
-        this.submit1 = true; // <<< Button shows if ID not found
+        this.submit1 = true; 
       },
 
       error =>{
         alert("Email number is already in use");
         this.SubmitData.Email = null;
-        this.submit1 = false;   // <<< Button hides when ID found
+        this.submit1 = false;  
       }
-      
     );
     }else{
-      this.submit1 = false;   // <<< Button hide
+      this.submit1 = false;  
     }
   }
 
-//==================================Post to api to see if email exists=============================
-  CheckEmail(){
 
-    
-    this.http.post("http://localhost:4200/api/UserExists", this.SubmitData.Email).subscribe( //  <<< Post for Email exists check
+  CheckEmail(){
+    this.http.post("http://localhost:4200/api/UserExists", this.SubmitData.Email).subscribe( 
       success => {
-        this.submit2 = true; // <<< Button shows if ID not found
+        this.submit2 = true; 
         if (this.SubmitData.Email.length == 0){
-          this.submit2 = false; // <<< Button hides 
+          this.submit2 = false;  
         }
       },
-
       error =>{
         alert("Email is already in use");
         this.SubmitData.Email = null;
         console.log(error);
-        this.submit2 = false; // <<< Button hides when ID found
+        this.submit2 = false; 
       }
-      
     );
-
   }
 
 
-  //====================================Loading bar for file upload===============================
   ngAfterViewInit(){
     this.settings.loadingSpinner = false; 
   }
-  //===========================================REGEX to avoid SQL injection=======================
+
   omit_special_char(event)
   {   
    var k;  
-   k = event.charCode;  //         k = event.keyCode;  (Both can be used)
+   k = event.charCode;  
    return((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57)); 
   }
-
-  //============================================REGEX only to allow numbers=======================
   omit_special_char_Let(event)
   {   
    var k;  
-   k = event.charCode;  //         k = event.keyCode;  (Both can be used)
+   k = event.charCode;  
    return( (k >= 48 && k <= 57)); 
   }
 }
